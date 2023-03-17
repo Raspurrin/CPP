6aaf12983468f29ceec47076bad64051be02507b
Djaisin <vondetou@gmail.com>
2023-03-17 23:06:46 +0100

    C++ style instead of C

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -2,17 +2,11 @@
 #include <stdint.h>
 #include <cctype>
 
-void	str_to_upper(char *str)
+void	str_upper(std::string str)
 {
-	size_t	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (isalpha(str[i]))
-			str[i] = (char)std::toupper(str[i]);
-		std::cout << str[i++];
-	}
+	for (size_t i = 0; i < str.size(); i++)
+		str[i] = std::toupper(str[i]);
+	std::cout << str;
 }
 
 int32_t main(int argc, char *argv[])
@@ -23,7 +17,10 @@ int32_t main(int argc, char *argv[])
 	if (argc < 2)
 		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 0);
 	while (argv[i])
-		str_to_upper(argv[i++]);
+	{
+		str_upper(argv[i++]);
+		std::cout << " ";
+	}
 	std::cout << std::endl;
 	return (0);
 }

01b1d07fc34e5b3713dd84b5df51e3fdfd920e43
Djaisin <vondetou@gmail.com>
2023-03-19 00:14:45 +0100

    It works now

--- module_01/ex04/replace.cpp
+++ module_01/ex04/replace.cpp
@@ -1,18 +1,19 @@
 #include "replace.hpp"
 
-std::string file_to_str(std::ifstream &path)
+void	replace(std::ifstream &infile, std::string infile_name, std::string toreplace, std::string replacement)
 {
-	std::ifstream	file(path);
-	return (std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>()));
-}
+	std::ofstream	outfile(infile_name.append(".replace"));
+	std::string		new_string;
 
-void	replace(std::ifstream &infile, char *toreplace, char *replacement)
-{
-	std::fstream	outfile;
-
-	std::string str = file_to_str(infile);
-	while (str.find != std::string::npos)
-	outfile.open("outfile", std::ios::out);
+	while (getline(infile, new_string))
+	{
+		for (size_t i = 0; (i = new_string.find(toreplace, i)) != new_string.npos;)
+		{
+			new_string.replace(i, toreplace.size(), replacement);
+			i += replacement.size();
+		}
+		outfile << new_string << std::endl;
+	}
 }
 
 // if stream no has copy constructor, so you cannot pass it by value
@@ -21,13 +22,14 @@ void	replace(std::ifstream &infile, char *toreplace, char *replacement)
 int32_t	main(int32_t argc, char **argv)
 {
 	std::ifstream	infile;
+	std::string		infile_name;
 
 	if (argc != 4)
 		return (std::cout << "You must provide 3 argumemts: a filename and two strings" << std::endl, 0);
 	infile.open(argv[1], std::ios::in);
 	if (!infile)
 		return(std::cout << "You used a wrong filename" << std::endl, 0);
-	replace(infile, argv[2], argv[3]);
+	replace(infile, argv[1], argv[2], argv[3]);
 	infile.close();
 	return (0);
 }

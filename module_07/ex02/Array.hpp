3d84ce911322354a242255c6e6a1c8d80887257c
raspurrin <vondetou@gmail.com>
2023-11-02 21:59:35 +0100

    module 7!

--- module_07/ex02/Array.hpp
+++ module_07/ex02/Array.hpp
@@ -1,7 +1,99 @@
+#pragma once
+
 #include <iostream>
+#include <exception>
 
 
-template <typename T>
+template<typename T>
+class Array
 {
+    private:
+        T       *array;
+        int  arraySize;
+    
+    public:
+        T       getElement(size_t elementNumber);
+        size_t     size();
 
+    public:
+        Array();
+        Array(unsigned int n);
+        Array(const Array &ref);
+        Array &operator=(const Array &ref);
+        T &operator[](int i);
+        ~Array();
+
+    class outOfBoundsException : std::exception
+    {
+        virtual const char *what() const throw()
+        {
+            return ("went out of bounds");
+        }
+    };
+};
+
+template<typename T>
+ T  Array<T>::getElement(size_t elementNumber)
+ {
+    return (array[elementNumber]);
+ }
+
+template<typename T>
+size_t Array<T>::size()
+{
+    return (arraySize);
 }
+
+template<typename T>
+Array<T>::Array()
+{
+    array = new T;
+    arraySize = 0;
+}
+
+template<typename T>
+Array<T>::Array(unsigned int n)
+{
+    array = new T[n];
+    arraySize = n;
+}
+
+template<typename T>
+Array<T>::Array(const Array &ref)
+{
+    array = new T[ref.arraySize];
+    arraySize = ref.arraySize;
+    for (size_t i = 0; i < (size_t)arraySize; ++i)
+        array[i] = ref.array[i];
+}
+
+template<typename T>
+Array<T> &Array<T>::operator=(const Array<T> &ref)
+{
+    delete[] array;
+    array = new T[ref.arraySize];
+    arraySize = ref.arraySize;
+    for (size_t i = 0; i < (size_t)arraySize; ++i)
+        array[i] = ref.array[i];
+    return (*this);
+}
+
+template<typename T>
+T &Array<T>::operator[](int elementIndex)
+{
+    if (elementIndex > arraySize || elementIndex < 0)
+        throw outOfBoundsException();
+    return array[elementIndex];
+}
+template<typename T>
+Array<T>::~Array()
+{
+    delete[] array;
+}
+
+// template<typename T>
+// std::ostream& operator<<(std::ostream& os, Array<T>& ref)
+// {
+//     os << ref.getElement(0);
+//     return (os);
+// }
--- module_07/ex02/main.cpp
+++ module_07/ex02/main.cpp
@@ -2,5 +2,32 @@
 
 int	main(void)
 {
-	return (0);
+    Array<int> emptyArray;
+    Array<std::string> stringArray(2);
+    stringArray[0] = "hi";
+    stringArray[1] = "yo";
+    std::cout << "stringarray[1]: " << stringArray[1] << std::endl;
+    Array<std::string> stringArrayCopy(stringArray);
+    std::cout << "stringarrayCopy[1]: " << stringArray[1] << std::endl;
+    std::cout << "Default Array Size: " << emptyArray.size() << std::endl;
+    std::cout << "---------------------------" << std::endl;
+    Array<int> intArray(3);
+    intArray[0] = 4;
+    intArray[1] = 6;
+    intArray[2] = 42;
+    std::cout << "intArray[2]: " << intArray[2] << std::endl;
+    std::cout << "intArray size: " << intArray.size() << std::endl;
+    emptyArray = intArray;
+    std::cout << "emptyArray[2]: " << emptyArray[2] << std::endl;
+    std::cout << "emptyArray size: " << emptyArray.size() << std::endl;
+    try
+    {
+        std::cout << "intArray[4]: " << intArray[4] << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+    return (0);
 }
